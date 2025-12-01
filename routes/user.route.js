ef76c07820950952ae470325e55f9fecd7cf1533
Ayush Singh <Ayush.Singh@parkar.local>
2025-12-01 12:32:33 +0530

    Final Change 1

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,10 +3,15 @@ import { adminOnly } from "../middlewares/auth.middleware.js";
 import { protect } from "../middlewares/auth.middleware.js";
 import {
     getUsers,
-    getUserById
+    getUserById,
+    updateUserRole // Import the new function
 } from '../controllers/user.controller.js';
 const router = express.Router();
 
-router.get("/",protect,adminOnly,getUsers);
-router.get("/:id",protect,getUserById);
+router.get("/", protect, adminOnly, getUsers);
+router.get("/:id", protect, getUserById);
+
+// *** NEW ROUTE ***
+router.put("/update-role/:userId", protect, adminOnly, updateUserRole);
+
 export default router;
